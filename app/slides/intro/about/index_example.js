71b1b1c56e4ee6477c976e47e19193d8a3a330d4
austin samsel <austinsamsel@gmail.com>
2017-06-17 15:21:44 -0400

    add intro slide

---
+++ app/slides/intro/about/index_example.js
@@ -0,0 +1,43 @@
+var about = [
+    {
+        name: 'DevMeetings',
+        who: 'passionate software developers',
+        what: 'organize offline and online workshops since 2011',
+        where: 'have been in Seattle, Barcelona, Oslo, Hamburg and 20 other cities around the world',
+    },
+    {
+        name: 'All Star Code',
+        who: 'a new non-profit organization',
+        what: 'dedicated to attracting, preparing and placing young men of color in careers within the tech sector',
+        where: 'NYC',
+        tags: ['sponsor', 'awesome', 'thank you!']
+    },
+    {
+        name: 'High Tops',
+        description: 'Software Engineering consultancy for fullstack Javascript development.',
+        owner: 'Austin Samsel',
+        twitter: '@austin_says____',
+        tags: ['react rulez']        
+    },
+    {
+        name: 'Workshop Links',
+        slides: 'https://htps-workshop-slides.surge.sh/',
+        sample_code: 'provided at the end',
+        survey: 'https://goo.gl/forms/kPCc1h240RJ85t4I2', 
+        // must fill out 1-2 hours before end of workshop.
+        chat: 'https://gitter.im/devmeetings/React-NYC'
+    },
+    {
+        name: 'Workshop Format',
+        theme: [
+            'Mix between a Hackathon (people implement an app) ', 
+            'workshop (short presentations of the mentors)'
+            ],
+        teams: 'Split into teams (3-4 persons per team)',
+        work: [
+            'Team works together on solutions, brainstorming etc, ',
+            'but each participant implements his own version of app'
+        ],
+    }
+]
+
